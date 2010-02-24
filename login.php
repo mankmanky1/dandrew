b3e53d9c92d519d9d33595de197e6710c87547c1
mankmanky1 <mankmanky1>
2010-02-24 19:17:34 +0200

    Tried out a template only on the login page, let me know what you think?

--- login.php
+++ login.php
@@ -1,3 +1,103 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<!-- This Site Was Designed By Wayne D. Fields http://illusivedesign.cjb.net -->
+<!-- Please make sure you read all of the read me file that came with this template then
+you may delete this comment -->
+<html>
+<head>
+<title>Student Submission System</title>
+<meta name="description" content="Insert Description Here">
+<meta name="keywords" content="Insert Keywords Here">
+<style type="text/css">
+<!--
+a:link, a:active, a:visited {
+color: #4A265A; 
+text-decoration: underline}
+
+a:hover {
+color: #4A265A; 
+text-decoration: none}
+
+body {
+background-image: URL(images/bg.gif);
+background-repeat: repeat-x}
+  
+-->
+</style>
+</head>
+<body bgcolor="#ffffff" marginwidth="0" marginheight="0" leftmargin="0" topmargin="0">
+
+<table border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td></td>
+<td><img src="images/eielogo.png" width="100" height="100" align = "center"></td>
+<!--CAN UNCOMMENT HERE<td><img src="images/mod_temp1_02.gif" width="265" height="105"></td>
+<td><img src="images/mod_temp1_03.gif" width="300" height="105"></td>-->
+
+</tr>
+<tr>
+<td colspan="3" background="images/bg_pg.gif" valign="top">
+
+<!--CAN UNCOMMENT HERE<table border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td width="10">&nbsp;</td>
+<td valign="top">
+
+<!-- Side Panel -->
+<!--CAN UNCOMMENT HERE
+<table border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td width="170" height="95">&nbsp;</td>
+</tr>
+
+<tr>
+<td><a href="#"><img src="images/link_1.gif" width="170" height="30" border="0" alt="link_1"></a></td>
+</tr>
+
+<tr>
+<td><a href="#"><img src="images/link_2.gif" width="170" height="30" border="0" alt="link_2"></a></td>
+</tr>
+
+<tr>
+<td><a href="#"><img src="images/link_3.gif" width="170" height="30" border="0" alt="link_3"></a></td>
+</tr>
+
+<tr>
+<td><a href="#"><img src="images/link_4.gif" width="170" height="30" border="0" alt="link_4"></a></td>
+</tr>
+
+<tr>
+<td><a href="#"><img src="images/link_5.gif" width="170" height="30" border="0" alt="link_5"></a></td>
+</tr>
+
+<tr>
+<td><a href="#"><img src="images/link_6.gif" width="170" height="30" border="0" alt="link_6"></a></td>
+</tr>
+
+<tr>
+<td width="170" height="214" valign="top">
+
+<br>
+<blockquote><font face="Arial" size="2" color="#000000"><strong>
+<img src="images/ballet_sm.gif" width="16" height="18" border="0">Site Info buttons ect.
+</strong></font></blockquote>
+
+</td>
+</tr>
+</table>
+<!-- Side Panel -->
+
+</td>
+<td width="40">&nbsp;</td>
+
+<td align="center" valign="top">
+
+<!-- Content --><br><br>
+<img src="images/hr_top.gif" width="528" height="58" border="0">
+
+<blockquote><font face="Arial" size="2" color="#000000"><strong>
+<img src="images/ballet_lg.gif" width="19" height="18" border="0">
+<!-- The login form-->
+<br></br>
 <form action='loginck.php' method=post>
 <table border='0' cellspacing='0' cellpadding='0' align=center>
 <tr id='cat'>
@@ -26,7 +126,23 @@ Member Sign UP</a></font></td> <td bgcolor='#ffffff' align='center'><font face='
 </td>
 </tr>
 </table></center>
-
-
-
 </form>
+<!--End of the login form-->
+<img src="images/ballet_lg.gif" width="19" height="18" border="0">
+</strong></font></blockquote>
+<br><br><br><br><br><br>
+
+<img src="images/hr_bot.gif" width="528" height="44" border="0">
+<!-- Content -->
+
+</td>
+</tr>
+</table>
+
+
+</td>
+</tr>
+</table>
+
+</body>
+</html>
