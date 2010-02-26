91aff74cdf47d4764d277f4d0f15af7d6c156666
Andrew Paverd <ajpaverd@gmail.com>
2010-02-26 20:21:22 +0200

    Added files to add and drop course columns

---
+++ add_course.php
@@ -0,0 +1,29 @@
+<?php
+@session_start();
+
+if (!(isset($_SESSION['login'])))
+{
+print "<script>";
+print " self.location='login.php';";
+print "</script>";
+exit();
+}
+
+$id = $_SESSION['id'];
+
+$course_code = $_SESSION['course_code'];
+$new_submission = $_POST['new_submission'];
+
+
+if(isset($course_code) && isset($new_submission))
+{
+include('connection.php');
+$query = "ALTER TABLE $course_code ADD $new_submission VARCHAR(80);";
+$result = mysql_query($query);
+}
+
+print "<script>";
+print " self.location='admin2.php';";
+print "</script>";
+
+
---
+++ delete_course.php
@@ -0,0 +1,29 @@
+<?php
+@session_start();
+
+if (!(isset($_SESSION['login'])))
+{
+print "<script>";
+print " self.location='login.php';";
+print "</script>";
+exit();
+}
+
+$id = $_SESSION['id'];
+
+$course_code = $_SESSION['course_code'];
+$deletion = $_POST['deletion'];
+
+
+if(isset($course_code) && isset($deletion))
+{
+include('connection.php');
+$query = "ALTER TABLE $course_code drop $deletion;";
+$result = mysql_query($query);
+}
+
+print "<script>";
+print " self.location='admin2.php';";
+print "</script>";
+
+
